0096b9cd4e476ac53cfce4a9ea3ed18ed4a55bd6
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-16 14:58:33 +0700

    first commit

---
+++ binarys.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int npanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan anyaknya element pada array (maksimal 10):"
+    }
+}
